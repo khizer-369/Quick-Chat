111e622fd8ed50634e590fb9d56de0a16c3b4116
khizer-369 <iamkhizar369@gmail.com>
2026-01-02 16:34:35 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-[url('./src/assets/bgImage.svg')] bg-contain border">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
